edb962b62aa79d66f9e1ac85066c99bc992007b7
zhiai <zhiai@qq.com>
2019-07-30 13:48:08 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'HollowWorld',
+    data () {
+      return {
+        title: 'HollowWorld 组件'
+      }
+    }
+  }
+</script>
+<style scoped>
+  h1 {
+    color: red
+  }
+</style>
