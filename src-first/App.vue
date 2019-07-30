959c085f89a94f1cac23bba6064bc9341daa1692
zhiai <zhiai@qq.com>
2019-07-30 17:04:22 +0800

    test

---
+++ src-first/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>APP</h1>
+    <img src="./assets/logo.png" alt="logo">
+    <HollowWorld />
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import HollowWorld from './components/HelloWorld.vue'
+  export default {
+    components: {
+      HollowWorld
+    }
+  }
+</script>
+<style scoped>
+  img {
+    width: 200px;
+    height: 200px;
+  }
+</style>
