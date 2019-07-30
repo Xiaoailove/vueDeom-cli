569a27bd0b05f49c25f950e751589a113cbf040a
zhiai <zhiai@qq.com>
2019-07-30 18:37:14 +0800

    静态页面完成

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <div>
-    <h1>APP</h1>
-    <img src="./assets/logo.png" alt="logo">
-    <HollowWorld />
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header></Header>
+      <List></List>
+      <Footer></Footer>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
-  import HollowWorld from './components/HelloWorld.vue'
-  export default {
-    components: {
-      HollowWorld
+    import Header from "./components/Header.vue"
+    import List from "./components/List.vue"
+    import Footer from "./components/Footer.vue"
+    export default {
+        components: {
+            Header,
+            List,
+            Footer
+        }
     }
-  }
 </script>
 <style scoped>
-  img {
-    width: 200px;
-    height: 200px;
-  }
+    .todo-container {
+        width: 600px;
+        margin: 0 auto;
+    }
+    .todo-container .todo-wrap {
+        padding: 10px;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+    }
 </style>
