9685abe984daa20acb7d8b4c89931705602d2b0b
zhiai <zhiai@qq.com>
2019-07-31 20:15:06 +0800

    用户搜索 ajax请求完成

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <template>
-  <div>
-      <h1 v-if="!reqName">加载中。。。。。。</h1>
-      <h1 v-else>
-          点赞量最多的仓库名称
-          <a :href="reqUrl">{{reqName}}</a>
-      </h1>
-  </div>
+    <div class="container">
+        <Search></Search>
+        <Main></Main>
+    </div>
 </template>
-
 <script type="text/ecmascript-6">
+    import Search from './components/Search'
+    import Main from './components/Main'
   export default {
-      data () {
-          return {
-              reqName:'',
-              reqUrl:''
-          }
-      },
-      mounted () {
-          const url='https://api.github.com/search/repositories?q=v&sort=stars'
-          this.$http.get(url).then((response)=>{
-              const result=response.data
-              const {name, html_url} = result.items[0]
-              //更新数据
-              this.reqName=name
-              this.reqUrl=html_url
-          }).catch(error=>{
-              alert('请求出错了'+error.statusText)
-          })
+      components:{
+          Search,
+          Main
       }
   }
 </script>
+<style scoped>
 
-<style  scoped>
-
- 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueResource  from 'vue-resource'
+import axios from 'axios'
 import App from './App'
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 Vue.use(VueResource)
+Vue.prototype.$bus=new Vue()
+Vue.prototype.$axios=axios
 new Vue({
   el: '#app',
   components: { App },
