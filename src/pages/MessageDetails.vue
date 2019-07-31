6ae56d05122b21ab9cc68391431f071a6187046a
zhiai <zhiai@qq.com>
2019-07-31 22:55:25 +0800

    路由传参完成

---
+++ src/pages/MessageDetails.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+      <li>id:{{$route.params.id}}</li>
+      <li>title:{{messageDetail.title}}</li>
+      <li>content:{{messageDetail.content}}</li>
+  </ul>
+</template>
+<script type="text/ecmascript-6">
+import { setTimeout } from 'timers';
+  //模拟后台服务器请求回来的数据
+  const messageDetails=[
+    {id: 1, title: 'message001', content: 'message content001'},
+    {id: 3, title: 'message003', content: 'message content003'},
+    {id: 5, title: 'message005', content: 'message content005'},
+  ]
+  export default {
+    data () {
+          return {
+            messageDetail:{}
+          }
+    },
+    mounted () {
+      setTimeout(()=>{
+        //得到当前页面所在的id
+        const id=this.$route.params.id*1
+        const details=messageDetails.find(detail=>detail.id===id)
+        this.messageDetail=details
+      },1000)
+    },
+    watch:{
+      $route (value) {
+        setTimeout(()=>{
+          const id=this.$route.params.id *1
+          const details=messageDetails.find(detail=>detail.id===id)
+          this.messageDetail=details
+        },1000)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,43 @@ import Vue from 'vue'
 import VueRouter from "vue-router"
 import About from "../pages/About.vue"
 import Home from "../pages/Home.vue"
+import News from "../pages/News.vue"
+import Message from "../pages/Message.vue"
+import MessageDetails from "../pages/MessageDetails.vue"
 Vue.use(VueRouter)
 export default new VueRouter({
     routes:[
-        {path:'/about',component:About},
-        {path:'/home',component:Home},
-        {path:'/',redirect:'/about'}
+        {
+            path:'/about',
+            component:About
+        },
+        {
+            path:'/home',
+            component:Home,
+            children:[
+                {
+                    path:'/home/news',
+                    component:News
+                },
+                {
+                    path:'/home/message',
+                    component:Message,
+                    children:[
+                        {
+                            path: '/home/message/detail/:id',
+                            component: MessageDetails
+                        }
+                    ]
+                },
+                {
+                    path:'/home',
+                    redirect:'/home/news'
+                }
+            ]
+        },
+        {
+            path:'/',
+            redirect:'/about'
+        }
     ]
 })
